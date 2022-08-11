3472cd35d6c991398114f4d736fe88affd2a04aa
Anton Jackson-Smith <anton@dyingstar.net>
2022-08-10 22:56:53 -0700

    New Polygon effects

--- src/main/java/storytime/lx/model/LampshadePolygon.java
+++ src/main/java/storytime/lx/model/LampshadePolygon.java
@@ -10,8 +10,29 @@ public class LampshadePolygon {
     public final List<LXPoint> points;
     public List<LampshadePolygon> neighbours = new ArrayList<>();
 
+    private double radius = 1;
+
     public LampshadePolygon(List<LXPoint> points) {
         this.points = Collections.unmodifiableList(points);
+
+        double maxDistance = 0;
+        for (LXPoint pa : points) {
+            for (LXPoint pb: points) {
+                maxDistance = Math.max(maxDistance, distn(pa, pb));
+            }
+        }
+        this.radius = maxDistance;
+    }
+
+    public static double distn(LXPoint a, LXPoint b) {
+        double dx = a.xn - b.xn;
+        double dy = a.yn - b.yn;
+        double dz = a.zn - b.zn;
+        return Math.sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    public double getRadius () {
+        return this.radius;
     }
 
     public void addNeighbour(LampshadePolygon neighbour) {
