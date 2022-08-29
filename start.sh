ba7aa81a99ac013020eb1975d5987cb29ca4a70f
Anton Jackson-Smith <anton@dyingstar.net>
2022-08-28 21:24:47 -0700

    all update preplaya

--- start.sh
+++ start.sh
@@ -2,9 +2,12 @@
 
 echo "Starting StorytimeLX"
 
+#HEADLESS="--headless"
+HEADLESS=""
+
 if [[ $DISPLAY == "" ]]; then
   echo "Setting display"
   export DISPLAY=:0.0
 fi
 
-java -cp 'target/lxstudio-ide-0.4.1-jar-with-dependencies.jar:lib/processing-4.0b8/*' -Djava.library.path=lib/processing-4.0b8/native heronarts.lx.app.LXStudioApp
+java -cp 'target/lxstudio-ide-0.4.1-jar-with-dependencies.jar:lib/processing-4.0b8/*' -Djava.library.path=lib/processing-4.0b8/native heronarts.lx.app.LXStudioApp $HEADLESS
